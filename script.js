9dc17665f03af2a784b247d32c1e831166c3efe6
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-10 11:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model); // Call the Car constructor
+    this.topSpeed = topSpeed;
+}
+
+// Set up prototype inheritance
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
